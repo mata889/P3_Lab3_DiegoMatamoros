f3ca3bd620051f0b5d328637408e87f76e4658bc
DiegoMatamoros <dam_900@unitec.edu>
2017-10-27 13:49:12 -0600

    se ha creado el menu

---
+++ Lab3_DiegoMatamoros.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+int menu;
+do{
+	cout<<"----menu----"<<endl
+	<<"1.Ejercicio#1"<<endl
+	<<"2.Ejercicio#2"<<endl
+	<<"3.Ejercicio#3"<<endl
+	<<"4.SALIR"<<endl
+	<<"introduzca su opcion";
+	cin>>menu;
+	switch(menu){
+	
+	case 1:{}break; 
+
+	case 2:{}break;
+
+	case 3:{}break;
+	}
+
+}while(menu!=4);
+
+return 0;
+
+}
